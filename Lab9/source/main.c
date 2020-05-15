b4592ed76741a1d6c8beaf242d768ca6940e78ac
Ubuntu <tyoo002@ucr.edu>
2020-05-14 19:40:54 -0700

    finished part 1

--- Lab9/source/main.c
+++ Lab9/source/main.c
@@ -8,10 +8,41 @@
  *      code, is my own original work.
  */
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
+volatile unsigned char TimerFlag = 0;
+
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+void TimerOn() {
+	TCCR1B = 0x0B;
+	OCR1A = 125;
+	TIMSK1 = 0x02;
+	TCNT1 = 0;
+
+	_avr_timer_cntcurr = _avr_timer_M;
+	SREG |= 0x80;
+}
+
+void TimerOff() { TCCR1B = 0x00; }
+void TimerISR() { TimerFlag = 1; }
+
+ISR(TIMER1_COMPA_vect) { 
+	_avr_timer_cntcurr--;
+	if(_avr_timer_cntcurr == 0) {
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
 void set_PWM(double frequency) {
 	static double current_frequency;
 	if(frequency != current_frequency) {
@@ -46,43 +77,40 @@ void Tick() {
 			state = Off;
 			break;
 		case Off:
-			if((~PINA & 0x01) == 1) {
+			if((~PINA & 0x07) == 1) {
                                 state = cPressed;
                         }
-                        else if((~PINA & 0x02) == 2) {
+                        else if((~PINA & 0x07) == 2) {
                                 state = dPressed;
                         }
-                        else if((~PINA & 0x04) == 4) {
+                        else if((~PINA & 0x07) == 4) {
                                 state = ePressed;
                         }
 
 			else {
 				state = Off;
 			}
+		
 			break;
 		case cPressed:
-			if((~PINA & 0x01) == 1) {
-				state = cPressed;
+			if((~PINA & 0x07) != 1) {
+				state = Off;;
 			}
-			else {
-				state = Off;
-			}
+			else { state = cPressed; }
 			break;
 		
 		case dPressed:
-			if((~PINA & 0x02) == 2) {
-				state = dPressed;
-			}
-			else {
+			if((~PINA & 0x07) != 2) {
 				state = Off;
 			}
+			else { state = dPressed; }
 			break;
 		case ePressed:
-			if((~PINA & 0x04) == 4) {
-				state = ePressed;
+			if((~PINA & 0x07) != 4) {
+				state = Off;;
 			}
 			else {
-				state = Off;
+				state = ePressed;
 			}
 			break;
 		default:
@@ -113,11 +141,12 @@ int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; 
     /* Insert your solution below */
 	PWM_on();
-	state = Start;
-	
+	TimerSet(20);
+	TimerOn();
     while (1) {
 	Tick();
-	
+	while(!TimerFlag);
+	TimerFlag = 0;
     }
     return 0;
 }
